0ad95e70941e04015e3ef9f920fe92a34e5a26da
xThrasherrr <thrashergamezz@gmail.com>
2023-08-13 00:03:39 -0600

    remove dumb mistake

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ description 'xt-repairs | xThrasherrr'
 author 'xThrasherrr'
 
 shared_scripts { '@ox_lib/init.lua', 'modules/shared.lua', 'shared/*.lua' }
-client_scripts { 'modules/client.lua', 'modules/client.lua', 'client/*.lua' }
-server_scripts { 'modules/server.lua', 'modules/server.lua', 'server/*.lua' }
+client_scripts { 'modules/client.lua', 'client/*.lua' }
+server_scripts { 'modules/server.lua', 'server/*.lua' }
 
 lua54 'yes'
