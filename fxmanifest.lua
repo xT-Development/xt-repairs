1ef3aaa53daa832c736653f1f1499e242116376a
xThrasherrr <thrashergamezz@gmail.com>
2024-02-28 18:43:13 -0700

    fix: missing bridge files in fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,5 +6,5 @@ lua54 'yes'
 description 'Repair Bays | xT Development'
 
 shared_scripts { '@ox_lib/init.lua', 'configs/shared.lua', 'shared/*.lua' }
-client_scripts { 'configs/client.lua', 'client/*.lua' }
+client_scripts { 'configs/client.lua', 'bridge/client/*.lua', 'client/*.lua' }
 server_scripts { 'configs/server.lua', 'bridge/server/*.lua', 'server/*.lua' }
